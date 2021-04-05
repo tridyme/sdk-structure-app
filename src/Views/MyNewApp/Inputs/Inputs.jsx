a2c54b26d652082bf5c2637c21c79bd8cff21009
triazur <guillaume.cassin@triazur.com>
2021-04-05 18:41:30 +0200

    save

--- src/Views/MyNewApp/Inputs/Inputs.jsx
+++ src/Views/MyNewApp/Inputs/Inputs.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ChartElem from '../../../Components/ChartElem';
-import InputElem from '../../../Components/OldInputElem';
+import InputElem from '../../../Components/InputElem';
 import CardElem from '../../../Components/CardElem';
 
 const useStyles = makeStyles(theme => ({
@@ -18,8 +18,8 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Inputs = ({
-  inputsData,
-  updateValue
+  values,
+  handleChangeValues
 }) => {
   const classes = useStyles();
 
@@ -31,16 +31,20 @@ const Inputs = ({
             title="My Inputs"
             subtitle="Section dimensions"
           >
-            <div>
-              <InputElem
-                data={inputsData.h}
-                updateValue={updateValue}
-              />
-              <InputElem
-                data={inputsData.b}
-                updateValue={updateValue}
-              />
-            </div>
+            <InputElem
+              value={values.b}
+              text={'b'}
+              description={'Largeur:'}
+              unit={'m'}
+              onChange={handleChangeValues('b')}
+            />
+            <InputElem
+              value={values.h}
+              text={'h'}
+              description={'Hauteur:'}
+              unit={'m'}
+              onChange={handleChangeValues('h')}
+            />
           </CardElem>
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -53,9 +57,9 @@ const Inputs = ({
                 chartTitle: 'Section Rectangulaire',
                 value: [
                   { x: 0, y: 0 },
-                  { x: inputsData.b.value, y: 0 },
-                  { x: inputsData.b.value, y: inputsData.h.value },
-                  { x: 0, y: inputsData.h.value },
+                  { x: values.b, y: 0 },
+                  { x: values.b, y: values.h },
+                  { x: 0, y: values.h },
                   { x: 0, y: 0 }
                 ],
                 axisName: { x: 'Largeur de la Section ', y: 'Hauteur de la section ' },
